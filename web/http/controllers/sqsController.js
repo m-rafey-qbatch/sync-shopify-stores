92e6f74e3e344eef5f823edb0a27d7823090f64d
Muhammad Rafey <m.rafey659@gmail.com>
2023-12-24 18:41:53 +0500

    update api

--- web/http/controllers/sqsController.js
+++ web/http/controllers/sqsController.js
@@ -1,7 +1,41 @@
+import dbConnect from "../../database/connection.js";
+import ShopifySessions from "../../database/models/shopify_sessions.js"
+import shopify from "../../shopify.js"
+dbConnect();
+
+
 export const processSqsMessage = async (req, res) => {
-  console.log("*".repeat(100));
-  console.log(req.body);
-  return res
-  .status(200)
-  .json({ body : req.body});;
-};
+  try {
+    const store = "cocomo001.myshopify.com"
+    const sessions = await ShopifySessions.find({})
+    // console.log(sessions)
+
+
+    const storesToBeUpdated = sessions.filter(x => x.shop !== store)
+
+    for (const session of storesToBeUpdated) {
+      const client = new shopify.api.clients.Graphql({ session });
+      console.log(client)
+
+// https://shopify.dev/docs/api/admin-graphql/2023-10/mutations/inventoryAdjustQuantity#examples-Increase_the_available_inventory_of_an_item_at_a_location_by_3
+    }
+    return res
+      .status(200)
+      .json({ body: req.body });;
+
+
+
+  } catch (e) {
+    console.log('*'.repeat(100))
+    console.log(e)
+  }
+
+
+}
+
+
+
+
+
+
+
